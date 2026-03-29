d4b4fb9f68fa8a5062bbe7c62df11a446762a1d2
seethelearni-coder <seethelearni@gmail.com>
2026-03-29 18:37:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     await update.message.reply_text(
         f"Привет, {user.first_name} 👋\n\n"
-        "Держи майнд-карту\n\n"
+        "Держи майнд-карту — система автопрогрева с помощью РСЯ.\n\n"
         "Приятного изучения и если захочешь разобрать как это работает именно в твоей нише — "
         "пиши: @vadimarket"
     )
@@ -19,7 +19,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_document(
             document=f,
             filename="Система заявок Вадим Альшин.xmind",
-            caption="7-шаговая система стабильных заявок для экспертов 🔥"
+            caption="Чтобы открыть файл правильно:\nСкачай Xmind на телефон или компьютер, открой файл с помощью данной программы.\n\nЕсли тебе лень даже на данном этапе, тебе не нужны клиенты и деньги)"
         )
 
 app = ApplicationBuilder().token(TOKEN).build()
