9b33da56305612be34a52654c95850c1af5f9b6e
seethelearni-coder <seethelearni@gmail.com>
2026-03-29 20:33:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,26 +18,9 @@ class Handler(BaseHTTPRequestHandler):
         pass
 
 def run_server():
-    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    PORT = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(("0.0.0.0", PORT), Handler)
     server.serve_forever()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    await update.message.reply_text(
-        f"Привет, {user.first_name} 👋\n\n"
-        "Держи майнд-карту — система автопрогрева с помощью РСЯ.\n\n"
-        "Приятного изучения и если захочешь разобрать как это работает именно в твоей нише — "
-        "пиши: @vadimarket"
-    )
-    with open(FILE_PATH, "rb") as f:
-        await update.message.reply_document(
-            document=f,
-            filename="Система заявок Вадим Альшин.xmind",
-            caption="Чтобы открыть файл правильно:\nСкачай Xmind на телефон или компьютер, открой файл с помощью данной программы.\n\nЕсли тебе лень даже на данном этапе, тебе не нужны клиенты и деньги)"
-        )
-
-threading.Thread(target=run_server, daemon=True).start()
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-print("Бот запущен...")
-app.run_polling()
+    user = update
